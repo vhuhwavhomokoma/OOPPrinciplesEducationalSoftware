316a34fd2fff5b55153213076fd86dfb0e4a8206
Agape <theaman249@gmail.com>
2024-10-28 20:46:03 +0200

    Added game 1 component

---
+++ frontend/master-oop/src/app/classes-online-game1/classes-online-game1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-classes-online-game1',
+  standalone: true,
+  imports: [],
+  templateUrl: './classes-online-game1.component.html',
+  styleUrl: './classes-online-game1.component.scss'
+})
+export class ClassesOnlineGame1Component {
+
+}
